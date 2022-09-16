176e2c76747fbd9b0d17e705a7e85d4f629c0c80
nosir <n@gmail.com>
2022-09-16 14:26:22 +0500

    JS

---
+++ script..js
@@ -0,0 +1,30 @@
+var name = prompt('Введите свое имя')
+var age = +prompt('Введите свой возраст')
+
+console.log(name);
+console.log(age);
+
+var exercise1st = +prompt ('100 + 100 = ')
+var exercise2nd = +prompt ('200 - 100 = ')
+var exercise3rd = +prompt ('100 * 10 = ')
+var exercise4th = +prompt ('100 / 2 = ')
+var exercise5th = +prompt ('100 % 15 = ')
+
+console.log('100 + 100 = ' + (100 + 100));
+console.log('Ваш ответ:' + exercise1st);
+console.log('200 - 100 = ' + (200 - 100));
+console.log('Ваш ответ:' + exercise2nd);
+console.log('100 * 10 = ' + (100 * 10));
+console.log('Ваш ответ:' + exercise3rd);
+console.log('100 / 2 = ' + (100 / 2));
+console.log('Ваш ответ:' + exercise4th);
+console.log('100 % 15 = ' + 100 % 15);
+console.log('Ваш ответ:' + exercise5th);
+
+var x = +prompt('Введите первое число')
+var y = +prompt('Введите второе число')
+var z = +prompt('Введите третье число')
+
+console.log(x);
+console.log(y);
+console.log(z);
